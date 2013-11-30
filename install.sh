b6cac6ec5a473b92ca7ac4667dcccc050a5dfb87
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2013-11-30 22:29:01 +0900

    initial my environment

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+source ~/.zshrc
---
+++ vimrc
@@ -0,0 +1,8 @@
+let g:molokai_original = 1
+:colorscheme molokai
+syntax on
+
+set tabstop=4
+set shiftwidth=4
+set number
+set title
